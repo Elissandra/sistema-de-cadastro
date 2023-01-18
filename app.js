4ed5ad9541cae699d3adc27bbf01cd1d8dbee8b3
Elissandra <oliver-lis@htomail.com>
2023-01-18 08:41:06 -0400

    console log indicando modificações do paulo

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("Eu fiz esse commit sem ngm ver na aula!")
 console.log("Enviando para o GitHub")
+console.log("modificação do paulo")
+
